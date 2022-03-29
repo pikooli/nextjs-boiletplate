716f0edd0e547aed761da533e401eabd0f42b08a
pikooli <>
2022-03-29 19:23:37 +0200

    adding tailwind

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,55 +1,47 @@
 import { useTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import Link from 'components/Link';
+import { Disclosure } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import NavLink from 'components/navbar/NavLink';
+
+const BtnMobile = ({ open }: { open: boolean }) => {
+  return (
+    <Disclosure.Button className="lg:hidden inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+      <span className="sr-only">Open main menu</span>
+      {open ? (
+        <XIcon className="block h-6 w-6" aria-hidden="true" />
+      ) : (
+        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+      )}
+    </Disclosure.Button>
+  );
+};
 
 export default function RootNavbar() {
   const { t } = useTranslation('navbar');
-  const router = useRouter();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <Link href="/">
-          <a className="navbar-brand">{t('title')}</a>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link href="/">
-                <a
-                  className={`nav-link ${router.pathname === '/' && 'active'}`}
-                  aria-current="page"
-                >
-                  {t('index')}
-                </a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/about">
-                <a
-                  className={`nav-link ${
-                    router.pathname === '/about' && 'active'
-                  }`}
-                  aria-current="page"
-                >
-                  {t('about')}
-                </a>
-              </Link>
-            </li>
-          </ul>
+    <Disclosure as="nav" className="navbar">
+      {({ open }) => (
+        <div>
+          <div className="flex justify-between">
+            <Link href="/" text="Pascal Zhang" className="text-2xl uppercase" />
+            <NavLink className="hidden lg:block" />
+            <BtnMobile open={open} />
+          </div>
+          {open ? <NavLink className="block lg:hidden" /> : null}
         </div>
-      </div>
-    </nav>
+      )}
+    </Disclosure>
   );
 }
+
+// <nav className="flex">
+//   <Link href="/">
+//     <a className="navbar-brand">{t('title')}</a>
+//   </Link>
+//   <BtnMobile />
+//   <div className="collapse navbar-collapse" id="navbarNav">
+//     <NavLink />
+//   </div>
+// </nav>;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
-import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
 
 import '../styles/globals.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import fontawersome from 'lib/fontawersome';
 import dayjs from 'lib/dayjs';
 
@@ -14,9 +12,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   dayjs.locale(router.locale);
   //
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap.bundle.min' as string);
-  }, []);
 
   return <Component {...pageProps} />;
 };
